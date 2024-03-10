0b4a7f3e99c95b17d6d2377de5a11c937f1b290d
Pavel Dalhou <dalhoupavel@gmail.com>
2024-03-10 16:59:42 +0100

    feat: add album dto an entity

--- src/album/dto/create-album.dto.ts
+++ src/album/dto/create-album.dto.ts
@@ -1,1 +1,25 @@
-export class CreateAlbumDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  ValidateIf,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  name: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  @ApiProperty()
+  year: number;
+
+  @IsUUID()
+  @ValidateIf((object, value) => value !== null)
+  @ApiProperty({ type: 'string', format: 'uuid', nullable: true })
+  artistId: string | null; // refers to Artist
+}
--- src/album/dto/update-album.dto.ts
+++ src/album/dto/update-album.dto.ts
@@ -1,4 +1,25 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAlbumDto } from './create-album.dto';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  ValidateIf,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateAlbumDto extends PartialType(CreateAlbumDto) {}
+export class UpdateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  name: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  @ApiProperty()
+  year: number;
+
+  @IsUUID()
+  @ValidateIf((object, value) => value !== null)
+  @ApiProperty({ type: 'string', format: 'uuid', nullable: true })
+  artistId: string | null; // refers to Artist
+}
