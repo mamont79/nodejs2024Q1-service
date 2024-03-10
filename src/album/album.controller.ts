c8e92d117909867cf77e752ffa488c0f51466d6c
Pavel Dalhou <dalhoupavel@gmail.com>
2024-03-10 17:38:56 +0100

    feat: add album servise

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -6,37 +6,62 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
+  NotFoundException,
+  Put,
+  HttpCode,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AlbumService } from './album.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { Album } from './entities/album.entity';
 
 @Controller('album')
+@ApiTags('Album')
 export class AlbumController {
   constructor(private readonly albumService: AlbumService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create album' })
+  @ApiResponse({ status: 201, type: Album })
   create(@Body() createAlbumDto: CreateAlbumDto) {
     return this.albumService.create(createAlbumDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all albums' })
+  @ApiResponse({ status: 200, type: [Album] })
   findAll() {
     return this.albumService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.albumService.findOne(id);
+  @ApiOperation({ summary: 'Get album with id' })
+  @ApiResponse({ status: 200, type: Album })
+  findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    const album = this.albumService.findOne(id);
+    if (!album) throw new NotFoundException(`Can't find album with id:  ${id}`);
+    return album;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlbumDto: UpdateAlbumDto) {
-    return this.albumService.update(id, updateAlbumDto);
+  @Put(':id')
+  @ApiOperation({ summary: 'Update album with id' })
+  @ApiResponse({ status: 200, type: Album })
+  update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() updateAlbumDto: UpdateAlbumDto,
+  ) {
+    const album = this.albumService.update(id, updateAlbumDto);
+    if (!album) throw new NotFoundException(`Can't find album with id:  ${id}`);
+    return album;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  @ApiOperation({ summary: 'Delete track with id' })
+  @ApiResponse({ status: 204 })
+  @HttpCode(204)
+  remove(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return this.albumService.remove(id);
   }
 }
