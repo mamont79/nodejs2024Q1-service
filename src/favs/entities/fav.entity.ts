233aa1d20974c4293a633a34a86eb8c7e324ba0f
Pavel Dalhou <dalhoupavel@gmail.com>
2024-03-11 01:17:21 +0100

    feat: add favs entity and service

--- src/favs/entities/fav.entity.ts
+++ src/favs/entities/fav.entity.ts
@@ -1,1 +1,15 @@
-export class Fav {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Album } from '../../album/entities/album.entity';
+import { Artist } from '../../artist/entities/artist.entity';
+import { Track } from '../../track/entities/track.entity';
+
+export class Fav {
+  @ApiProperty({ type: [Artist] })
+  artists: Artist[];
+
+  @ApiProperty({ type: [Album] })
+  albums: Album[];
+
+  @ApiProperty({ type: [Track] })
+  tracks: Track[];
+}
--- src/favs/favs.module.ts
+++ src/favs/favs.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FavsService } from './favs.service';
 import { FavsController } from './favs.controller';
+import { AlbumService } from '../album/album.service';
+import { ArtistService } from '../artist/artist.service';
+import { DbService } from '../db/db.service';
+import { TrackService } from '../track/track.service';
 
 @Module({
   controllers: [FavsController],
-  providers: [FavsService]
+  providers: [
+    FavsService,
+    TrackService,
+    AlbumService,
+    ArtistService,
+    DbService,
+  ],
 })
 export class FavsModule {}
