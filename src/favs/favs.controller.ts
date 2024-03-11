e5e9881f9b146c0caa49a9707b393b93e12c4daf
Pavel Dalhou <dalhoupavel@gmail.com>
2024-03-11 08:32:08 +0100

    feat: add favs controller

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -1,34 +1,79 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  HttpCode,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { FavsService } from './favs.service';
 import { CreateFavDto } from './dto/create-fav.dto';
 import { UpdateFavDto } from './dto/update-fav.dto';
+import { Fav } from './entities/fav.entity';
+import { Track } from '../track/entities/track.entity';
+import { Artist } from '../artist/entities/artist.entity';
+import { Album } from '../album/entities/album.entity';
 
 @Controller('favs')
+@ApiTags('Favourites')
 export class FavsController {
   constructor(private readonly favsService: FavsService) {}
 
-  @Post()
-  create(@Body() createFavDto: CreateFavDto) {
-    return this.favsService.create(createFavDto);
-  }
-
   @Get()
+  @ApiOperation({ summary: 'Get all favourites' })
+  @ApiResponse({ status: 200, type: Fav })
   findAll() {
     return this.favsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.favsService.findOne(+id);
+  @Post('artist/:id')
+  @ApiOperation({ summary: 'Add artist to favourites' })
+  @ApiResponse({ status: 201, type: Artist })
+  addFavArtist(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favsService.addFavArtist(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFavDto: UpdateFavDto) {
-    return this.favsService.update(+id, updateFavDto);
+  @Delete('artist/:id')
+  @ApiOperation({ summary: 'Remove artist from favourites' })
+  @ApiResponse({ status: 204 })
+  @HttpCode(204)
+  removeFavArtist(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+  ) {
+    return this.favsService.removeFavArtist(id);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.favsService.remove(+id);
+  @Post('track/:id')
+  @ApiOperation({ summary: 'Add track to favourites' })
+  @ApiResponse({ status: 201, type: Track })
+  addFavTrack(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favsService.addFavTrack(id);
+  }
+
+  @Delete('track/:id')
+  @ApiOperation({ summary: 'Remove track from favourites' })
+  @ApiResponse({ status: 204 })
+  @HttpCode(204)
+  removeFavTrack(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favsService.removeFavTrack(id);
+  }
+
+  @Post('album/:id')
+  @ApiOperation({ summary: 'Add album to favourites' })
+  @ApiResponse({ status: 201, type: Album })
+  addFavAlbum(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favsService.addFavAlbum(id);
+  }
+
+  @Delete('album/:id')
+  @ApiOperation({ summary: 'Remove album from favourites' })
+  @ApiResponse({ status: 204 })
+  @HttpCode(204)
+  removeFavAlbum(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favsService.removeFavAlbum(id);
   }
 }
